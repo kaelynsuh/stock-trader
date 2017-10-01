8e0460f917d53d52c2cc71ec52a73eef67c63c37
Kaelyn Suh <kaelyn.suh@gmail.com>
2017-10-01 15:12:13 -0400

    Create and structure components

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>The Home Component</h1>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+</style>
---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>The Portfolio - Stock Component</h1>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+</style>
