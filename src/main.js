3f78e3c4505e526ccbdc5117709a3cd31f16655f
Kaelyn Suh <kaelyn.suh@gmail.com>
2017-10-01 15:27:56 -0400

    Install vue-router and configure routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
+
 import App from './App.vue'
+import { routes } from './routes';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history', // to get rid of #
+  routes
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
