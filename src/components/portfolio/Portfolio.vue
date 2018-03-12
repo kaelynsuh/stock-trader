ea65a0bfb8642c68a899bfbaa4da60ffa092b52f
Kaelyn Suh <kaelyn.suh@gmail.com>
2018-03-11 22:16:52 -0400

    Set up Portfolio - Stock with vuex

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,9 +1,21 @@
-<template>
-  <h1>The Portfolio Component</h1>
+<template lang="pug">
+  div
+    app-stock(v-for="stock in stocks", :stock="stock")
 </template>
 
 <script>
+  import {mapGetters} from 'vuex';
+  import Stock from './Stock.vue';
+
   export default {
+    computed: {
+      ...mapGetters({
+        stocks: 'stockPortfolio'
+      })
+    },
+    components: {
+      appStock: Stock
+    }
   }
 </script>
 
--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,9 +1,43 @@
-<template>
-  <h1>The Portfolio - Stock Component</h1>
+<template lang="pug">
+  div.col-md-4.col-sm-6
+      div.card.mb-3
+        .card-header.text-white.bg-success {{ stock.name }}
+          small  (Price: {{ stock.price }} | Quantity: {{ stock.quantity }})
+        .card-body
+          .card-text
+            .float-left
+              input.form-control(type="number", 
+                                placeholder="Quantity", 
+                                v-model.number="quantity")
+            .float-right
+              button.btn.btn-success(@click="sellStock", 
+                                    :disabled="quantity <= 0 || !Number.isInteger(quantity)") 
+                                    | Sell
 </template>
 
 <script>
+  import {mapActions} from 'vuex';
+
   export default {
+    props: ['stock'],
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+    methods: {
+      ...mapActions([
+        'sellStock'
+      ]),
+      sellStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        };
+        this.sellStock();
+      }
+    }
   }
 </script>
 
