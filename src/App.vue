d8cca6b3be43ff917ce21714ad3682014d1ba839
Kaelyn Suh <kaelyn.suh@gmail.com>
2018-03-21 21:55:05 -0400

    Animate route transitions

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     app-header
     .row
       .col
-        router-view
+        transition(name="slide", mode="out-in")
+          router-view
 </template>
 
 <script>
@@ -22,5 +23,34 @@
 <style>
   body {
     padding: 2rem;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 200ms ease-out forwards;
+  }
+  .slide-leave-active {
+    animation: slide-out 200ms ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(-2rem);
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+      opacity: 1;
+    }
+    to {
+      transform: translateY(-2rem);
+      opacity: 0;
+    }
   }
 </style>
