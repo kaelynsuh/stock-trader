652a5385a916f0de262698a8484b1cdd64afdf9d
Kaelyn Suh <kaelyn.suh@gmail.com>
2018-03-25 20:40:06 -0400

    Set up firebase

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import store from './store/store'
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
+Vue.http.options.root = "https://vuejs-stock-trader-748ae.firebaseio.com/";
+
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
 })
